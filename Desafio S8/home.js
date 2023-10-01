8039968eedf6652c6111efae3b50f82a73da9ef4
Viviana Teng <132222712+viviiteng@users.noreply.github.com>
2023-10-01 20:56:05 -0300

    original document

---
+++ Desafio S8/home.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Escribe el codigo necesario para acceder al nombre del usuario y guardarlo localmente.
+  // Si el usuario se loguea mostrar mensaje de bienvenida y si no lo hace mostrar que complete los campos.
+});
+    
---
+++ Desafio S8/login.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const loginForm = document.getElementById("loginForm");
+
+    loginForm.addEventListener("submit", function(event) {
+      event.preventDefault();    
+
+     
+        // Almacenar el nombre en sessionStorage para usarlo en home.js
+        
+        // Redirigir a la página principal
+      
+    });
+
+});
